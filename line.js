1c5d4430bea05973f41d245161cadf81246e9f90
jvahle <48984317+webflowjvahle@users.noreply.github.com>
2022-12-22 20:14:47 +0100

    Portfolio

--- line.js
+++ line.js
@@ -1,1 +1,26 @@
+const navClients = document.querySelector("[data-clients]");
+navClients.addEventListener("click", startLineAnimation);
+function startLineAnimation() {
+  // Split text and GSAP Animation
+  let typeSplit;
 
+  // Split the text up
+  function runSplit() {
+    typeSplit = new SplitType(".split-lines", {
+      types: "lines, words",
+    });
+    $(".line").append("<div class='line-mask'></div>");
+    createAnimation();
+  }
+  runSplit();
+
+  function createAnimation() {
+    $(".line").each(function (index) {
+      let tl = gsap.timeline();
+      tl.to($(this).find(".line-mask"), {
+        width: "0%",
+        duration: 5,
+      });
+    });
+  }
+}
